53a28ee977910ea737772baedf2940a9b846bef2
kiv1n <aloha@kiv1n.ru>
2023-05-16 17:01:12 +0300

    Rename "slug" variable to "address" for account page

--- app/pages/accounts/[address].tsx
+++ app/pages/accounts/[address].tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
  */
 export default function Account() {
   const router = useRouter();
-  const { slug } = router.query;
+  const { address } = router.query;
 
   return <Layout>...</Layout>;
 }
